ad6e624ebbd7a6db5e70bb6ffc1c0dd3c92ff460
Daniel Carazo <danielcarazo20@gmail.com>
2021-07-26 18:38:02 -0400

    set up express and heroku

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const app = express ();
 const PORT = process.env.PORT || 3001;
 const { animals } = require('./data/animals.json');
 
-
-
-
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -44,15 +41,22 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
   
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-})
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById (req.params.id, animals);
+  if (result) {
+  res.json(result);
+  } else {
+    res.send(404)
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}!`);
