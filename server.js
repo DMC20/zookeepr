9baaeebe7d635157654f08b34c8e94d2d285631f
Daniel Carazo <danielcarazo20@gmail.com>
2021-07-30 15:05:53 -0400

    routed front end html

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const { animals } = require('./data/animals');
 
 const fs = require('fs');
 const path = require('path');
-const { type } = require('os');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +11,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -102,6 +103,18 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
